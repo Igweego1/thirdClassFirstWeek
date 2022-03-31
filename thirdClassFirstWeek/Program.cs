b8f5661fe6f3956337071bf8b1d35294c72cf573
RESIDENT FINTECH <RESIDENT FINTECH@DESKTOP-E1I7AMP>
2022-03-31 10:38:48 +0100

    Add project files.

---
+++ thirdClassFirstWeek/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace thirdClassFirstWeek
+{
+    internal class Program
+    {
+        static void Main()
+
+        {
+            int[] numbers = new int[] { 9, 3, 88, 45 };
+
+            numbers[3] = numbers[3] + 1;
+            numbers[2] = numbers[0];
+
+            Console.WriteLine(numbers[1]);
+
+            bool[] boolarray = new bool[] { true, true, true };
+
+            
+
+        }
+    }  
+}
